be85e51cf0d39ef215e8add11be864549c70bc54
D-sherrily <1132917088@qq.com>
2020-07-03 16:33:22 +0800

    组中的第K个最大元素&数字范围按位与

---
+++ src/main/java/FindKthLargest.java
@@ -0,0 +1,38 @@
+import java.lang.reflect.Array;
+import java.util.*;
+import java.lang.Object;
+/**
+ * @ClassName: findKthLargest
+ * @Description: 数组中的第K个最大元素
+ * @author: D
+ * @create: 2020-06-29 17:51
+ **/
+
+public class FindKthLargest {
+    public static int findKthLargest(Vector<Integer> nums, int k) {
+        Object[] array = nums.stream().distinct().toArray();
+        Arrays.sort(array);
+
+        return (int)array[k-1];
+    }
+
+    public static int findKthLargest(int[] nums, int k) {
+
+        Arrays.sort(nums);
+        return nums[nums.length-k];
+
+    }
+
+    public static void main(String[] args) {
+        Vector<Integer> vector = new Vector<>();
+        vector.add(1);
+        vector.add(2);
+        vector.add(2);
+        vector.add(3);
+        int i = findKthLargest(vector, 2);
+        System.out.println(i);
+    }
+}
+
+
+
---
+++ src/main/java/RangeBitwiseAnd.java
@@ -0,0 +1,46 @@
+import java.math.BigInteger;
+
+/**
+ * @ClassName: RangeBitwiseAnd
+ * @Description: 数字范围按位与
+ * @author: D
+ * @create: 2020-07-01 16:09
+ **/
+
+public class RangeBitwiseAnd {
+    /**
+     * 题目：给定范围 [m, n]，其中 0 <= m <= n <= 2147483647，返回此范围内所有数字的按位与（包含 m, n 两端点）。
+     * 解答：暴力破解容易类型长度不够
+     *          当 n>m 那么 m - n之间肯定有多于一位的数
+     *          eg:
+     *          2,4
+     *          2:0 1 0     0 1         0
+     *          3:0 1 1     0 1         0
+     *          4:1 0 0     1 0         1
+     *                   0        0            0     0
+     *
+     *          与运算  有0为0   全1为1
+     *          如果n > m 那么m n 的末尾间隔 10101  01010  肯定有0
+     *          所以只要n > m 那么最后一位肯定为0   用 i 记录 0 的个数
+     *          然后右移 m n  继续比较
+     *          直到 m = n 那么直接取 剩下 m 的值  就可以结束比较
+     *          将剩下的 m 的值左移留出0 的位置  即得到结果
+     *
+     **/
+    public static int rangeBitwiseAnd(int m, int n) {
+       int i = 0;
+       while (m < n){
+          m>>>=1;
+          n>>>=1;
+          i++;
+       }
+        return m<<i;
+    }
+
+
+    public static void main(String[] args) {
+        Integer i = rangeBitwiseAnd(2, 4);
+        System.out.println(i);
+
+    }
+}
