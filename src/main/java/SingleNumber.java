7081f5dd5ffdd3e7597f37ac0f219f558a825c27
D-sherrily <1132917088@qq.com>
2020-07-03 18:17:55 +0800

    只出现一次的数字

---
+++ src/main/java/SingleNumber.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @ClassName: SingleNumber
+ * @Description: 只出现一次的数字
+ * @author: D
+ * @create: 2020-07-03 17:23
+ **/
+
+public class SingleNumber {
+    public static int[] singleNumber(int[] nums) {
+/*        List<Object> list = new ArrayList<>();
+        Collections.addAll(list, nums);
+        List<Object> listDist = list.stream().distinct().collect(Collectors.toList());
+        list.removeAll(listDist);
+        Object[] array = list.stream().toArray();
+
+        return array;*/
+        return null;
+    }
+
+
+}
